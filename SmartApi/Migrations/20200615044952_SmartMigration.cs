93257fa4d0a8839280bcb37b2a4cc3c738e87594
NaviD6935 <navid.shatel@gmail.com>
2020-06-15 09:25:03 +0430

    990326

---
+++ SmartApi/Migrations/20200615044952_SmartMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartApi.Migrations
+{
+    public partial class SmartMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- SmartApi/Models/SmartContext.cs
+++ SmartApi/Models/SmartContext.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SmartApi.Models;
 
 namespace SmartApi.Models
 {
 
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Users> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("data source=.;initial catalog=CoreAppDb;integrated security = true");
+            optionsBuilder.UseSqlServer("data source=192.168.0.58;Initial Catalog=SmartApi;user = sa;password=69655.6935.;MultipleActiveResultSets=true;");
         }
+
+        public DbSet<SmartApi.Models.Users> Users { get; set; }
     }
 }
